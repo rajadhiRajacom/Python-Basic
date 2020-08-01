7e2134c21ba3ab3f9a27f3b0ef577523bef8fa11
RajadhiRaja <raja.qtp14@gmail.com>
2020-08-01 17:53:23 +0530

    Create 07_List_TupleTest.py
    Adding list and tuple case

---
+++ 07_List_TupleTest.py
@@ -0,0 +1,9 @@
+list_of_tuples = [ ("1", "Chennai"), ("2", "Bangalore") ]
+print (list_of_tuples)
+
+list_of_tuples.insert(1, (1,2,3,4,5))
+print (list_of_tuples)
+
+tuple_of_lists = ( [1,2,3,4,5] , ["a","b","c","d","e"])
+
+print (tuple_of_lists)
